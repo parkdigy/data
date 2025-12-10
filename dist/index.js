e910ab6fe52e1d8ba52e66af3d66c09bf8490ce1
parkdigy <parkdigy@gmail.com>
2025-12-10 22:24:22 +0900

    1.0.4

--- dist/index.js
+++ dist/index.js
@@ -5,49 +5,18 @@
  * ******************************************************************************************************************/
 function copy(value) {
     return JSON.parse(JSON.stringify(value));
-}/******************************************************************************
-Copyright (c) Microsoft Corporation.
-
-Permission to use, copy, modify, and/or distribute this software for any
-purpose with or without fee is hereby granted.
-
-THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
-REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
-AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
-INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
-LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
-OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
-PERFORMANCE OF THIS SOFTWARE.
-***************************************************************************** */
-/* global Reflect, Promise, SuppressedError, Symbol, Iterator */
-
-
-var __assign = function() {
-    __assign = Object.assign || function __assign(t) {
-        for (var s, i = 1, n = arguments.length; i < n; i++) {
-            s = arguments[i];
-            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p)) t[p] = s[p];
-        }
-        return t;
-    };
-    return __assign.apply(this, arguments);
-};
-
-typeof SuppressedError === "function" ? SuppressedError : function (error, suppressed, message) {
-    var e = new Error(message);
-    return e.name = "SuppressedError", e.error = error, e.suppressed = suppressed, e;
-};/********************************************************************************************************************
+}/********************************************************************************************************************
  * 구현
  * ******************************************************************************************************************/
 function lv(label, value, other) {
-    return __assign(__assign({}, other), { label: label, value: value });
+    return Object.assign(Object.assign({}, other), { label, value });
 }/********************************************************************************************************************
  * 구현
  * ******************************************************************************************************************/
 function vl(value, label, other) {
-    return __assign(__assign({}, other), { value: value, label: label });
+    return Object.assign(Object.assign({}, other), { value, label });
 }var index = {
-    copy: copy,
-    lv: lv,
-    vl: vl,
+    copy,
+    lv,
+    vl,
 };exports.copy=copy;exports.default=index;exports.lv=lv;exports.vl=vl;
