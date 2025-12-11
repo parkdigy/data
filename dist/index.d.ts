10f6ab3a4a80905cfad2d05274b3d6516d15497d
parkdigy <parkdigy@gmail.com>
2025-12-11 16:28:55 +0900

    1.0.5

---
+++ dist/index.d.ts
@@ -0,0 +1,12 @@
+export * from './copy';
+export * from './lv';
+export * from './vl';
+import copy from './copy';
+import lv from './lv';
+import vl from './vl';
+declare const _default: {
+    copy: typeof copy;
+    lv: typeof lv;
+    vl: typeof vl;
+};
+export default _default;
